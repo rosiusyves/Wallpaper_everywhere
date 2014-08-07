9c722a994db4d87cb36693c1430bb2b0ec731989
Rosius Yves <rosiusyves@yahoo.co.uk>
2014-08-07 08:57:34 +0000

    Changelog 0.1:
    * Added draft file.
    * Problem mapping.

---
+++ Wallpaper_Everywhere.py
@@ -0,0 +1,36 @@
+'''
+Created on Aug 5, 2014
+
+@author: Yves.Rosius
+'''
+
+import os
+import platform
+
+print os.name
+print platform.system()
+print platform.release()
+
+# Linux: Check if GUI is started.
+#   Check if GUI is started.
+
+# Windows:
+#   Roaming profile or local profile?
+#   Registry hacks?
+#   Group policies?
+
+# Windows 8 +:
+#   Onedrive profile sync?
+
+# Macintosh:
+#   Pffffrrt
+
+# Android:
+#   Version compatibility
+#
+
+# Windows Phone:
+#   No wallpaper option available.
+
+# iOS: Pffffrrt
+
